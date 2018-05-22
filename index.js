0f20440621302fc0742effa488735edc2dcbeaf8
Forrest Benton <elliottbenton@gmail.com>
2018-05-22 22:33:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
 }
 
 function deleteFromObjectByKey(object,key) {
-  var clone = Object.assign({},object)
+  var clone = Object.assign({},object,key)
   delete clone.key 
   return clone
 }
